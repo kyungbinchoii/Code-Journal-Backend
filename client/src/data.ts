2c9956c2c37f4fee43c104635575f23ff954efb6
Johan Sangalang <jnsangalang93@yahoo.com>
2024-06-07 18:21:14 +0000

    johan

--- client/src/data.ts
+++ client/src/data.ts
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export type Entry = {
   entryId?: number;
   title: string;
@@ -9,62 +11,78 @@ type Data = {
   entries: Entry[];
   nextEntryId: number;
 };
+// const [error, setError] = useState<unknown>();
 
 const dataKey = 'code-journal-data';
+// function readData(): Data {
+  //   let data: Data;
+  //   const localData = localStorage.getItem(dataKey);
+  //   if (localData) {
+    //     data = JSON.parse(localData);
+    //   } else {
+      //     data = {
+        //       entries: [],
+        //       nextEntryId: 1,
+        //     };
+        //   }
+        //   return data;
+        // }
 
-function readData(): Data {
-  let data: Data;
-  const localData = localStorage.getItem(dataKey);
-  if (localData) {
-    data = JSON.parse(localData);
-  } else {
-    data = {
-      entries: [],
-      nextEntryId: 1,
-    };
-  }
-  return data;
-}
-
-function writeData(data: Data): void {
-  const dataJSON = JSON.stringify(data);
-  localStorage.setItem(dataKey, dataJSON);
-}
 
 export async function readEntries(): Promise<Entry[]> {
-  return readData().entries;
+
+    const response = await fetch('/api/entries');
+    if (!response) {
+      throw new Error('failed to fetch entries');
+    }
+    return await response.json();
 }
 
 export async function readEntry(entryId: number): Promise<Entry | undefined> {
-  return readData().entries.find((e) => e.entryId === entryId);
+    const response = await fetch(`/api/entries/${entryId}`);
+    if(!response){
+      throw new Error(`failed to fetch entry ${entryId}`);
+    }
+    const entry = await response.json()
+    return entry
 }
 
 export async function addEntry(entry: Entry): Promise<Entry> {
-  const data = readData();
-  const newEntry = {
-    ...entry,
-    entryId: data.nextEntryId++,
-  };
-  data.entries.unshift(newEntry);
-  writeData(data);
-  return newEntry;
+
+    const response = await fetch('/api/entries', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(entry),
+    });
+
+    if (!response) {
+      throw new Error('failed to fetch entries');
+    }
+    const addedEntry = await response.json();
+    return addedEntry
 }
 
 export async function updateEntry(entry: Entry): Promise<Entry> {
-  const data = readData();
-  const newEntries = data.entries.map((e) =>
-    e.entryId === entry.entryId ? entry : e
-  );
-  data.entries = newEntries;
-  writeData(data);
-  return entry;
+  const response = await fetch(`api/entries/${entry.entryId}`,{
+    method: 'PUT',
+    headers:{'Content-Type':'application/json'},
+    body: JSON.stringify(entry)
+  });
+  if(!response) {
+    throw new Error('failed to update entry')
+  }
+  const updatedEntry = await response.json();
+  return updateEntry
 }
 
 export async function removeEntry(entryId: number): Promise<void> {
-  const data = readData();
-  const updatedArray = data.entries.filter(
-    (entry) => entry.entryId !== entryId
-  );
-  data.entries = updatedArray;
-  writeData(data);
+  const response = await fetch(`/api/entries/${entryId}`,{
+    method: 'DELETE'
+  });
+  if(!response){
+    throw new Error('failed to delete entry');
+  }
+  const deletedEntry = await response.json()
+  return deletedEntry;
+
 }
--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ import 'dotenv/config';
 import pg from 'pg';
 import express from 'express';
 import { ClientError, errorMiddleware } from './lib/index.js';
+import { time } from 'console';
+import { title } from 'process';
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -12,6 +14,116 @@ const db = new pg.Pool({
 });
 
 const app = express();
+app.use(express.json());
+
+app.get('/api/entries', async (req,res,next)=>{
+  try{
+    const sql = `
+      select *
+      from "entries"
+      order by "entryId" desc
+      `;
+    const result = await db.query(sql);
+    if(!result){
+      throw new ClientError(400, 'invalid entries');
+    }
+    res.json(result.rows);
+
+  } catch (err){
+    next(err);
+  }
+})
+app.get('/api/entries/:entryId', async (req,res,next)=>{
+  try{
+    const {entryId} = req.params
+    const sql = `
+      select *
+      from "entries"
+      where "entryId" = $1
+      `;
+    const result = await db.query(sql, [entryId]);
+    if(!result){
+      throw new ClientError(400, 'invalid entries');
+    }
+    res.json(result.rows);
+
+  } catch (err){
+    next(err);
+  }
+})
+
+app.post('/api/entries', async (req,res,next)=>{
+  try{
+    const {title, notes, photoUrl} = req.body
+    if(!title){
+      throw new ClientError(400, `${title} is invalid`);
+    }
+    if(!notes){
+      throw new ClientError(400, `${notes} is invalid`);
+    }
+    if(!photoUrl){
+      throw new ClientError(400, `${photoUrl} is invalid`);
+    }
+    const sql = `
+    insert into "entries" ("title", "notes", "photoUrl")
+    values($1, $2, $3)
+    returning *
+    `;
+
+    const result = await db.query(sql, [title,notes,photoUrl]);
+    if(!result){
+      throw new ClientError(400, `invalid entry`);
+    }
+    const [entry] = result.rows
+    res.status(201).json(entry);
+  }catch(err){
+    next(err)
+  }
+})
+
+app.put('/api/entries/:entryId', async (req,res,next)=>{
+  try{
+    const {entryId} = req.params
+    const {title,notes,photoUrl} = req.body
+    const sql = `
+    update "entries"
+    set "title" = $1,
+        "notes" = $2,
+        "photoUrl" = $3
+        where "entryId" = $4
+      returning *;
+      `;
+      const result = await db.query(sql, [title,notes, photoUrl, entryId]);
+      if(!result){
+        throw new ClientError(400, `the ${entryId} was invalid`)
+      }
+      const [updatedEntry] = result.rows
+      res.status(201).json(updatedEntry)
+  } catch(err){
+      next(err)
+  }
+})
+
+app.delete('/api/entries/:entryId', async (req,res,next)=>{
+  try{
+    const {entryId} = req.params
+    const sql = `
+    delete from "entries"
+      where "entryId" = $1
+      returning *;
+      `;
+      const result = await db.query(sql, [entryId]);
+      if(!result){
+        throw new ClientError(400, `the ${entryId} was invalid`)
+      }
+      res.sendStatus(204)
+  } catch(err){
+      next(err)
+  }
+})
+
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
